c3dbd943937dd4f042a53d6d7528eaa6ce99c7d8
Shahin Alam <shahinzzz1949@gmail.com>
2022-11-09 05:40:34 +0600

    single service fetch by data

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,9 +27,26 @@ async function run (){
         const reviewCollection = client.db('assignment11db').collection('reviews');
 
         // fetch all foods
-        app.get('/all-food', async (req, res) => {
+        app.get('/services', async (req, res) => {
             const result = await foodCollection.find({}).toArray();
             res.send(result);
+        });
+         // fetch 3 foods
+         app.get('/services-3', async (req, res) => {
+            const result = await foodCollection.find({}).limit(3).toArray();
+            res.send(result);
+        });
+         // fetch data by id
+         app.get('/each-service', async (req, res) => {
+            const id = await req.query.id;
+            if(id !== ''){
+                const condition = {_id : ObjectId(id)};
+                const result = await foodCollection.find(condition).toArray();
+                console.log("query id : ", result);
+                res.send(result);
+            }
+            
+            
         });
         // fetch all reviews
         app.get('/reviews', async (req, res) => {
